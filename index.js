eb530cde42207c9d59789eefcc926d56fc2779e6
Emil Lystimaki <emilaleksanteri@gmail.com>
2023-04-10 15:19:50 +0100

    multi route

--- index.js
+++ index.js
@@ -1,19 +1,59 @@
-const { createServer } = require('http');
-const express = require('express');
-const WebSocket = require('ws');
-const app = express();
-app.use(express.json({ extended: false }));
-app.use(express.static('public'));
+import { createServer } from 'http';
+import { parse } from 'url';
+import WebSocket, { WebSocketServer } from 'ws';
 
-var port = process.env.PORT || 3000;
-const server = new WebSocket.Server({ server: app.listen(port) });
+const server = createServer();
+const wss1 = new WebSocketServer({ noServer: true });
+const wss2 = new WebSocketServer({ noServer: true });
 
-server.on('connection', (socket) => {
-  socket.on('message', (msg) => {
-    server.clients.forEach((client) => {
+wss1.on('connection', function connection(ws) {
+  ws.on('error', console.error);
+
+  // chat data
+  ws.on('message', () => {
+    wss1.clients.forEach(function each(client) {
       if (client.readyState === WebSocket.OPEN) {
-        client.send('message');
+        client.send('msg');
       }
     });
   });
+
+  ws.on('close', () => {
+    ws.close();
+  });
 });
+
+wss2.on('connection', function connection(ws) {
+  ws.on('error', console.error);
+
+  // typing, takes in username, don't send data to self
+  ws.on('message', (data, isBinary) => {
+    wss2.clients.forEach(function each(client) {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(data, { binary: isBinary });
+      }
+    });
+  });
+
+  ws.on('close', () => {
+    ws.close();
+  });
+});
+
+server.on('upgrade', function upgrade(request, socket, head) {
+  const { pathname } = parse(request.url);
+
+  if (pathname === '/chat') {
+    wss1.handleUpgrade(request, socket, head, function done(ws) {
+      wss1.emit('connection', ws, request, socket);
+    });
+  } else if (pathname === '/typing') {
+    wss2.handleUpgrade(request, socket, head, function done(ws) {
+      wss2.emit('connection', ws, request, socket);
+    });
+  } else {
+    socket.destroy();
+  }
+});
+
+server.listen(3003);
