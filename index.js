26a420ef98deacb3adc3ede88317a125c527915e
Emil Lystimaki <emilaleksanteri@gmail.com>
2023-04-11 12:15:26 +0100

    sound effect socket

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ import WebSocket, { WebSocketServer } from 'ws';
 const server = createServer();
 const wss1 = new WebSocketServer({ noServer: true });
 const wss2 = new WebSocketServer({ noServer: true });
+const wss3 = new WebSocketServer({ noServer: true });
 
+// chat socket
 wss1.on('connection', function connection(ws) {
   ws.on('error', console.error);
 
@@ -32,6 +34,7 @@ wss1.on('connection', function connection(ws) {
   }, 600000);
 });
 
+// typing socket
 wss2.on('connection', function connection(ws) {
   ws.on('error', console.error);
 
@@ -57,6 +60,32 @@ wss2.on('connection', function connection(ws) {
   }, 600000);
 });
 
+// sound effect socker
+wss3.on('connection', function connection(ws) {
+  ws.on('error', console.error);
+
+  // typing, takes in username, don't send data to self
+  ws.on('message', (data, isBinary) => {
+    wss3.clients.forEach(function each(client) {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(data, { binary: isBinary });
+      }
+    });
+  });
+
+  ws.on('close', () => {
+    ws.close();
+  });
+
+  setInterval(() => {
+    wss3.clients.forEach(function each(client) {
+      if (client.readyState === WebSocket.CLOSED) {
+        client.close();
+      }
+    });
+  }, 600000);
+});
+
 server.on('upgrade', function upgrade(request, socket, head) {
   const { pathname } = parse(request.url);
 
@@ -67,6 +96,10 @@ server.on('upgrade', function upgrade(request, socket, head) {
   } else if (pathname === '/typing') {
     wss2.handleUpgrade(request, socket, head, function done(ws) {
       wss2.emit('connection', ws, request, socket);
+    });
+  } else if (pathname === '/sounds') {
+    wss3.handleUpgrade(request, socket, head, function done(ws) {
+      wss3.emit('connection', ws, request, socket);
     });
   } else {
     socket.destroy();
